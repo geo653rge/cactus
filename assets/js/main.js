00e2ee931c8d4a1080bb203300746024c3a5b1ce
geo653rge <georgekimanikiringa@gmail.com>
2024-04-03 10:23:37 +0300

    adding javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,35 @@
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+//  SHOW MENU //
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+      /* Menu show */
+
+      if(navToggle){
+
+        navToggle.addEventListener('click' ,() =>{
+            navMenu.classList.add('show-menu')
+        })
+      }
+      if(navClose){
+
+        navToggle.addEventListener('click' ,() =>{
+            navMenu.classList.remove('show-menu')
+        })
+      }
+
+
+// REMOVE MENU MOBILE
+
+const navlink = document.querySelectorAll('nav-link')
+
+const linkAction =() =>{
+
+const navMenu = document.getElementById('nav-menu')
+navlink.classList.remove('show-menu')
+}
+ navlink.forEach(n => n.addEventListener('click', linkAction))
