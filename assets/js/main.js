e40ee47f5e05fbc0a4cfaa08919bce79113a680f
geo653rge <georgekimanikiringa@gmail.com>
2024-06-18 22:18:25 +0300

    adding index html part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+// const navMenu = document.getElementById("nav-menu"),
+//   navToggle = document.getElementById("nav-toggle"),
+//   navClose = document.getElementById("nav-close");
 
-/*Menu show */
+// /*Menu show */
 
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
-}
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
-}
+// if (navToggle) {
+//   navToggle.addEventListener("click", () => {
+//     navMenu.classList.add("show-menu");
+//   });
+// }
+// if (navClose) {
+//   navClose.addEventListener("click", () => {
+//     navMenu.classList.remove("show-menu");
+//   });
+// }
