54fd6a7bfd3786040288e385962ea724a6641f39
geo653rge <georgekimanikiringa@gmail.com>
2024-07-10 16:35:53 +0300

    adding scroll button for both up and down

--- js/main.js
+++ js/main.js
@@ -18,23 +18,36 @@ if (navClose) {
 }
 // REMOVE MENU MOBILE
 
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-// when each of the item on the nav__link is clicked , the show-menu closes.
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n =>n.addEventListener('click',linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // when each of the item on the nav__link is clicked , the show-menu closes.
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=======ADD BLUR HEADER========*/
 
-const blurHeader = () =>{
-  const header = document.getElementById('header')
+const blurHeader = () => {
+  const header = document.getElementById("header");
 
-  // Add a class if the bottom offset is greater than 50 of the 
-  this.scrollY >= 50 ? header.classList.add('blur-header')
-                   :header.classList.remove('blur-header')
-}
+  // Add a class if the bottom offset is greater than 50 of the
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
 
-window.addEventListener('scroll',blurHeader)
+window.addEventListener("scroll", blurHeader);
+
+// SHOW SCROLL UP
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  //when the scroll is higher than 350 viewprt height , add the show-scroll to the a tag with the scrollup class
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
